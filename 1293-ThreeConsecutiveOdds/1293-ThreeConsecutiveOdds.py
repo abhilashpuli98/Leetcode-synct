96aeabfb6010e2d1c114f3ba2992a7a9ff165615
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:41:07 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.py
@@ -0,0 +1,17 @@
+# Last updated: 2/5/2026, 7:41:07 AM
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count=0
+        indx= None
+        for i in range (0,len(arr)-2):
+            if arr[i]%2!=0 and  arr[i+1]%2!=0 and arr[i+2]%2!=0:
+                return True
+        return False 
+            
+                
+        
+            
