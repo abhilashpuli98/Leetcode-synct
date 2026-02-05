65cffdd0ef15404dd9800b41067d9dbe1096b3a4
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:39:21 +0530

    Create 3782-FindValidEmails

---
+++ 3782-FindValidEmails/3782-FindValidEmails.sql
@@ -0,0 +1,5 @@
+-- Last updated: 2/5/2026, 7:39:21 AM
+# Write your MySQL query statement below
+select user_id,email from users
+where email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$'
+order by user_id
