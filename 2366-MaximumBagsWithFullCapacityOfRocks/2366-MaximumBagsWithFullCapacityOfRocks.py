30dc32cef6e381c35dd15d606d11389964c3824e
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:40:21 +0530

    Create 2366-MaximumBagsWithFullCapacityOfRocks

---
+++ 2366-MaximumBagsWithFullCapacityOfRocks/2366-MaximumBagsWithFullCapacityOfRocks.py
@@ -0,0 +1,19 @@
+# Last updated: 2/5/2026, 7:40:21 AM
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        fullCap=0
+        need=[]
+        for i in range(len(capacity)):
+            if capacity[i]==rocks[i]:
+                fullCap+=1
+            else:
+                need.append(capacity[i]-rocks[i])
+        if sum(need)<=additionalRocks:
+            return len(need)+fullCap
+        need.sort()
+        for i in range(len(need)):
+            if additionalRocks<need[i]:
+                break
+            additionalRocks-=need[i]
+            fullCap+=1
+        return fullCap
