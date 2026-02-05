34a9a9d5122052d89e747a050049c85be5c3b91f
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:39:59 +0530

    Create 3383-TakingMaximumEnergyFromTheMysticDungeon

---
+++ 3383-TakingMaximumEnergyFromTheMysticDungeon/3383-TakingMaximumEnergyFromTheMysticDungeon.py
@@ -0,0 +1,9 @@
+# Last updated: 2/5/2026, 7:39:59 AM
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        maxEnergy = float('-inf')
+        energySequence = [0]*len(energy)
+        for x in range(len(energy)-1,-1,-1):
+            energySequence[x] = energy[x]+(energySequence[x+k] if x+k<len(energy) else 0)
+            maxEnergy = max(maxEnergy,energySequence[x])
+        return maxEnergy
