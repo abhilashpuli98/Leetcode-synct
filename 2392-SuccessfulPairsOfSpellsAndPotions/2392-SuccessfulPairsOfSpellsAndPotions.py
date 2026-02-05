c91e2792f8fa300aecdf143361c87e5c315773d0
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:40:17 +0530

    Create 2392-SuccessfulPairsOfSpellsAndPotions

---
+++ 2392-SuccessfulPairsOfSpellsAndPotions/2392-SuccessfulPairsOfSpellsAndPotions.py
@@ -0,0 +1,17 @@
+# Last updated: 2/5/2026, 7:40:17 AM
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        result=[]
+        potions.sort()
+        for s in spells:
+            index=len(potions)
+            l,r=0,len(potions)-1
+            while l<=r:
+                mid=(l+r)//2
+                if s*potions[mid]>=success:
+                    r=mid-1
+                    index=mid
+                else:
+                    l=mid+1 #i forgot to sub add +1,-1 from mid i became mad and set r-=1,l+=1
+            result.append(len(potions)-index)
+        return result
