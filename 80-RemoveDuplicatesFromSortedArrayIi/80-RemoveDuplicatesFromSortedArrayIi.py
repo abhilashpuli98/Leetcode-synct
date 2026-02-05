d561bef727dfc36787a60c4e31747fba78a147f4
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:43:55 +0530

    Create 80-RemoveDuplicatesFromSortedArrayIi

---
+++ 80-RemoveDuplicatesFromSortedArrayIi/80-RemoveDuplicatesFromSortedArrayIi.py
@@ -0,0 +1,15 @@
+# Last updated: 2/5/2026, 7:43:55 AM
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left=0
+        right=1
+        n=len(nums)
+        freqDict={}
+        k=0
+        for num in (nums):
+            freqDict[num]=freqDict.get(num,0)+1
+            if freqDict[num]<=2:
+                nums[k]=num
+                k+=1
+        return k
+        
