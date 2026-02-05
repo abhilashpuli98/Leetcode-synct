c7a690b83646dafc9a64204c3456c4da8ee1e2c7
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:39:31 +0530

    Create 3705-FindTheLargestAlmostMissingInteger

---
+++ 3705-FindTheLargestAlmostMissingInteger/3705-FindTheLargestAlmostMissingInteger.py
@@ -0,0 +1,16 @@
+# Last updated: 2/5/2026, 7:39:31 AM
+class Solution:
+    def largestInteger(self, nums: List[int], k: int) -> int:
+        if k==len(nums):
+            return max(nums)
+        result=-1
+        counter=Counter(nums)
+        if k==1:
+            return max((num for num in counter if counter[num]==1),default=-1)
+        if counter[nums[0]]==1:
+            result=max(nums[0],result)
+        if counter[nums[-1]]==1:
+            result=max(nums[-1],result)
+        return result 
+        
+        
