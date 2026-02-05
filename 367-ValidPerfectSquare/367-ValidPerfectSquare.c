6801f5626a6fd215cb5299a285f7f65550927899
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:42:15 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.c
@@ -0,0 +1,20 @@
+// Last updated: 2/5/2026, 7:42:15 AM
+bool isPerfectSquare(int n) {
+    long mid,low=0,high=n-1;
+    while(low<=high)
+    {
+        mid=low+(high-low)/2;
+        if(n==0||n==1)
+        return true;
+        if(mid*mid==n)
+        return true;
+        else if(mid*mid<n)
+        low=mid+1;
+        else
+        high=mid-1;
+
+    }
+    return false;
+
+    
+}
