bf75f3a778c09f96cdc16267d0c71f94620b85d1
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:40:33 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.py
@@ -0,0 +1,17 @@
+# Last updated: 2/5/2026, 7:40:33 AM
+class Solution(object):
+    def isThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        count=0
+        for i in range(1,n+1):
+            if n%i==0:
+                count+=1
+        if count!=3:
+             return False
+        return True
+
+
+        
