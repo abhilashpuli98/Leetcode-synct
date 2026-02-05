dbc2ae9fe38a0823cef245e65b44858d98db6c82
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:40:58 +0530

    Create 1449-PrintWordsVertically

---
+++ 1449-PrintWordsVertically/1449-PrintWordsVertically.py
@@ -0,0 +1,17 @@
+# Last updated: 2/5/2026, 7:40:58 AM
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        words=s.split()
+        result=[]
+        max_length=max(len(word) for word in words)
+        for i in range(max_length):
+            current_word=""
+            for j in range(len(words)):
+                if i<len(words[j]):
+                    current_word+=words[j][i]
+                else:
+                    current_word+=" "
+            result.append(current_word.rstrip())
+        return result
+       
+            
