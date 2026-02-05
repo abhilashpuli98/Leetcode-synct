271d6cc8ec10e7c9039329b6910de4b851120d6d
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:39:16 +0530

    Create 3799-Unique3DigitEvenNumbers

---
+++ 3799-Unique3DigitEvenNumbers/3799-Unique3DigitEvenNumbers.py
@@ -0,0 +1,15 @@
+# Last updated: 2/5/2026, 7:39:16 AM
+class Solution:
+    def totalNumbers(self, nums: List[int]) -> int:
+        freq=Counter(nums)
+        count=0
+        for i in range(100,1000):
+            h=i//100
+            t=(i//10)%10
+            u=(i)%10
+            if u%2!=0:
+                continue
+            need=Counter([h,t,u])
+            if all(freq[d]>=need[d] for d in need):
+                count+=1
+        return count
