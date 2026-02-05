a43dde8bec4da31f71fd6cf14328983bc747711b
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:40:00 +0530

    Create 2977-CheckIfAStringIsAnAcronymOfWords

---
+++ 2977-CheckIfAStringIsAnAcronymOfWords/2977-CheckIfAStringIsAnAcronymOfWords.py
@@ -0,0 +1,15 @@
+# Last updated: 2/5/2026, 7:40:00 AM
+class Solution(object):
+    def isAcronym(self, words, s):
+        """
+        :type words: List[str]
+        :type s: str
+        :rtype: bool
+        """
+        if len(words)!=len(s):
+            return False
+        for i in range(0,len(s)):
+            if(words[i][0]!=s[i] or words[i]==0):
+                return False
+        return True
+            
