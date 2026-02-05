a2936a25d30709058d44cf4526844503a507cb9a
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:41:06 +0530

    Create 1353-FindResultantArrayAfterRemovingAnagrams

---
+++ 1353-FindResultantArrayAfterRemovingAnagrams/1353-FindResultantArrayAfterRemovingAnagrams.py
@@ -0,0 +1,6 @@
+# Last updated: 2/5/2026, 7:41:06 AM
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        result=[words[0]]
+        result.extend([words[i] for i in range(1,len(words)) if sorted(words[i]) != sorted(words[i-1]) ])
+        return result
