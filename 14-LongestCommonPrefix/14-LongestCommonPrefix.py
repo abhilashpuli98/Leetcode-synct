953764d8932fdca0d25059e25b75cc0f86854e35
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:44:45 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,24 @@
+# Last updated: 2/5/2026, 7:44:45 AM
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        strs.sort()
+        p=0
+        q=0
+        m=0
+        n=len(strs)-1
+        temp=min(len(strs[m]),len(strs[n]))
+        prefix=""
+        while(temp):
+            if strs[m][p]==strs[n][q]:
+                prefix+=strs[m][p]
+                p+=1
+                q+=1
+                temp-=1
+            elif strs[m][p]!=strs[n][q]:
+                return prefix
+        return prefix
+        
