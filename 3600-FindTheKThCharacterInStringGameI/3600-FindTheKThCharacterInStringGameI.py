52b61ff80ff26636ce98ac55dd7683ecd9946f01
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:39:37 +0530

    Create 3600-FindTheKThCharacterInStringGameI

---
+++ 3600-FindTheKThCharacterInStringGameI/3600-FindTheKThCharacterInStringGameI.py
@@ -0,0 +1,10 @@
+# Last updated: 2/5/2026, 7:39:37 AM
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        count=0
+        res=['a']
+        while len(res)<k:
+            for i in range(len(res)):
+                let=chr(ord('a')+((ord(res[i])-ord('a')+1)%26))
+                res.append(let)
+        return res[k-1]
