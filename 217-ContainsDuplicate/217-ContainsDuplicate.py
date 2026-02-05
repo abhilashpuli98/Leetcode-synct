9114529588124e1b3aee42019a4240a3a5d80569
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:42:45 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -0,0 +1,10 @@
+# Last updated: 2/5/2026, 7:42:44 AM
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq=set()
+        for i in range(len(nums)):
+            if nums[i] in freq:
+                return True
+            else:
+                freq.add(nums[i])
+        return False
