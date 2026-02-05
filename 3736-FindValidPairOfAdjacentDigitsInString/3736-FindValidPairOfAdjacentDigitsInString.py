05bb9927b8f84d16c84081ad59a0669635519e39
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:39:27 +0530

    Create 3736-FindValidPairOfAdjacentDigitsInString

---
+++ 3736-FindValidPairOfAdjacentDigitsInString/3736-FindValidPairOfAdjacentDigitsInString.py
@@ -0,0 +1,9 @@
+# Last updated: 2/5/2026, 7:39:27 AM
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        s = list(s)
+        freq = Counter(s)
+        for i in range(1,len(s)):
+            if s[i-1]!=s[i] and int(s[i]) == freq[s[i]] and int(s[i-1])==freq[s[i-1]]:
+                return s[i-1]+s[i]
+        return ""
