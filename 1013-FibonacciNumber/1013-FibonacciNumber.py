d4ffa4baba05c577611230fc9fae22435235fb90
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:41:21 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.py
@@ -0,0 +1,10 @@
+# Last updated: 2/5/2026, 7:41:21 AM
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<2:
+            return n
+        result=[0,1]
+        for i in range(2,n+1):
+            temp=sum(result)
+            result[0],result[1]=result[1],temp
+        return result[-1]
