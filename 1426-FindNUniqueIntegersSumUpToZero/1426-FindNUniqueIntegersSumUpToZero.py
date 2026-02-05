42fe8bdf633c1e7d3fec2a61e5a3326608bdaa7f
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:41:01 +0530

    Create 1426-FindNUniqueIntegersSumUpToZero

---
+++ 1426-FindNUniqueIntegersSumUpToZero/1426-FindNUniqueIntegersSumUpToZero.py
@@ -0,0 +1,12 @@
+# Last updated: 2/5/2026, 7:41:01 AM
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        #return [-(n*(n-1))//2]+list(range(1,n))
+        res=[]
+        for i in range(1,(n//2)+1):
+            res.append(i)
+            res.append(-i)
+        if n%2==1:
+            res.append(0)
+        return res
+        
