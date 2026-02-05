b4e614511c98ca998682ba14d9ad285f60626fad
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-05 07:43:25 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.py
@@ -0,0 +1,14 @@
+# Last updated: 2/5/2026, 7:43:25 AM
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        result=[]
+        finalres=0
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1] and nums[i] not in result:
+                result.append(nums[i])
+        for i in range(len(nums)):
+            result.append(nums[i])
+        for i in range(len(result)):
+            finalres^=result[i]
+        return finalres
